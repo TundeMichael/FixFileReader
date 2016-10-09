dbf81b842463fb6f57f78f2f236939a80ad02f5a
TundeMichael <tundemichaeltm@gmail.com>
2016-10-09 10:15:20 +0100

    Update ApplicationConfig.java

--- src/main/java/com/tundemicahel/seamfix/ApplicationConfig.java
+++ src/main/java/com/tundemicahel/seamfix/ApplicationConfig.java
@@ -24,7 +24,7 @@ public class ApplicationConfig extends Application {
      * If required, comment out calling this method in getClasses().
      */
     private void addRestResourceClasses(Set<Class<?>> resources) {
-        resources.add(com.tundemicahel.seamfix.SubscribersResource.class);
+        resources.add(com.tundemichael.reader.SubscribersResource.class);
     }
     
 }
